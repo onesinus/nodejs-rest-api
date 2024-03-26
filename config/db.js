16fa7d29826ea9d21353382da38da53ad806484a
Onesinus Tamba <onesinus231@gmail.com>
2024-03-26 23:42:05 +0700

    ...

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb+srv://admin:admin@onesinus.wdkycn8.mongodb.net', {})
+// mongoose.connect('mongodb+srv://<username>:<password>@onesinus.wdkycn8.mongodb.net', {})
 mongoose.connect('mongodb://localhost:27017', {})
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
