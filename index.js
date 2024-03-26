81e353e3353ce5c299bf9dec04705d07743fe287
Onesinus Tamba <onesinus231@gmail.com>
2024-03-27 01:05:38 +0700

    setup cors

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const jobRoutes = require('./routes/jobRoutes');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors()); // Enable CORS
+
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
